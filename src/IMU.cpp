329a6747601c7662b81c9c9d51e38e1cbf6571f7
Zachary Lineman <zachary.lineman@gmail.com>
2023-02-23 21:30:49 -0500

    Revert "IMU Can Dictionary"
    This reverts commit fa3bf4642688678f7424d5dc60396acc54902524.

--- src/IMU.cpp
+++ src/IMU.cpp
@@ -1,11 +1,3 @@
 #include <IMU.hpp>
 
-namespace IMU {
-CO_OBJ_T* IMU::getObjectDictionary() {
-    return objectDictionary;
-}
-
-uint16_t IMU::getObjectDictionarySize() const {
-    return OBJECT_DICTIONARY_SIZE;
-}
-}
+namespace IMU {}
